73b1c8f22e8f4bc79ebca74dae74f834d7e6c06e
Anton <Cevleron@yandex.ru>
2023-01-22 20:35:15 +0300

    add parallelMatrix

---
+++ ParallelMatrix/Program.cs
@@ -0,0 +1,59 @@
+﻿const int N = 1000; // размер матрицы
+
+int[,] serialMulRes = new int[N, N]; // результат умножения матриц в однопотоке
+int[,] threadMulRes = new int[N, N]; // результат паралельного умножения матриц
+
+int[,] firstMatrix = MatrixGenerator(N, N);
+int[,] secondMatrix = MatrixGenerator(N, N);
+
+SerialMatrixMul(firstMatrix, secondMatrix);
+
+int[,] MatrixGenerator(int rows, int columns)
+{
+    Random _rand = new Random();
+    int[,] res = new int[rows, columns];
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            res[i, j] = _rand.Next(-100, 100);
+        }
+    }
+    return res;
+}
+
+void SerialMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new System.Exception("Нельзя умножать такие матрицы"); // проверка на схожесть матриц
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                serialMulRes[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+}
+
+void PrepareParallelMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Нельзя умножить такие матрицы");
+    int eachThreadCalc = N / THREADS_NUMBER;
+    Thread[] arr = new Thread[2];
+    var threadsList = new List<Thread>();
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        int startPos = i * eachThreadCalc;
+        int endPos = (i + 1) * eachThreadCalc;
+        //если последний поток
+        if (i == THREADS_NUMBER - 1) endPos = N;
+        threadsList.Add(new Thread(() => ParallelMatrixMul(a, b, startPos, endPos)));
+        threadsList[i].Start();
+    }
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        threadsList[i].Join();
+    }
+}
